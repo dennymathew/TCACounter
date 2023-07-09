543da234c6fb3d57ee829aba2e946f7a825df177
Denny Mathew <devdenmathew@gmail.com>
2023-07-09 23:34:51 +0200

    Initial Commit

---
+++ TCACounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCACounter
+//
+//  Created by Denny on 09/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCACounter/TCACounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterApp.swift
+//  TCACounter
+//
+//  Created by Denny on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TCACounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
